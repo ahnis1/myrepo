ce80bbc1f8bd28bd511593bc766658857ab8a585
Ubuntu <ubuntu@ip-172-31-48-117.ec2.internal>
2023-03-01 18:20:43 +0000

    updated readme

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3030;
+
+app.get('/', (req, res) => res.send('Hello World! This is Nodejs running on Ec2 Docker instance'))
+
+app.listen(port, (err) => {
+    if (err) {
+      console.log('Error::', err);
+    }
+      console.log(`Nodejs app listening on port ${port}`);
+  });
